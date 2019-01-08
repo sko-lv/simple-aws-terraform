f83cd0eb4b9e4bf7663fd040c7f67e811d043d91
usov_boris <busov@ukr.net>
2019-01-08 14:07:47 +0200

    Added DynamoDB

--- main.tf
+++ main.tf
@@ -4,6 +4,22 @@ provider "aws" {
     secret_key = "${var.aws_secret_key}"
 }
 
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+ 
+  tags {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+}
+
 resource "random_pet" "s3b" {
 }
 
