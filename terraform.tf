6935b5ea9e9e1b4cd6c0d5680482be6c974dbd13
Oleh Skoropad <notaruta@gmail.com>
2019-01-08 19:04:51 +0200

    Fixed tfstate s3 support

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
  backend "s3" {
  encrypt = true
  bucket = "s3tfstate"
- dynamodb_table = "terraform-state-lock-dynamo"
+# dynamodb_table = "terraform-state-lock-dynamo"
  region = "us-east-1"
  key = "tfstate/lock.tf"
  }
